14e8bda374f1f5f110243ae0619e2f9e9c129183
Antibody <antibody@yandex.ru>
2018-04-25 10:11:59 +0300

    Create MostSimpleToken.sol

---
+++ MostSimpleToken.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.23;
+
+contract MostSimpleToken {
+   
+    mapping (address => uint256) public balanceOf;
+
+    
+    constructor () {                                 //instead of function, no warnings in Solidity ^0.4.23
+        balanceOf[msg.sender] = 21000000;              
+    }
+
+ 
+    function transfer(address _to, uint256 _value) public {
+        require(balanceOf[msg.sender] >= _value);           
+        require(balanceOf[_to] + _value >= balanceOf[_to]); 
+        balanceOf[msg.sender] -= _value;                    
+        balanceOf[_to] += _value;                           
+    }
+}
